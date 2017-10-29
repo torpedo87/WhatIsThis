aaf2017c04b4faaa9cbfccdc6bc12a43d6edc88b
torpedo87 <bestofhand@naver.com>
2017-10-29 10:32:19 +0900

    Initial Commit

---
+++ WhatIsThis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatIsThis
+//
+//  Created by junwoo on 2017. 10. 29..
+//  Copyright © 2017년 samchon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
