b882eda25f194e2b3263c5986918835060823bb3
torpedo87 <bestofhand@naver.com>
2017-10-29 12:44:00 +0900

    capture photo

--- WhatIsThis/Controller/CameraVC.swift
+++ WhatIsThis/Controller/CameraVC.swift
@@ -14,6 +14,7 @@ class CameraVC: UIViewController {
   var captureSession: AVCaptureSession!
   var cameraOutput: AVCapturePhotoOutput!
   var previewLayer: AVCaptureVideoPreviewLayer!
+  var photoData: Data?
 
   @IBOutlet weak var roundedLabelView: RoundedShadowView!
   @IBOutlet weak var cameraView: UIView!
@@ -36,6 +37,10 @@ class CameraVC: UIViewController {
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     
+    let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+    tap.numberOfTapsRequired = 1
+    
+    
     captureSession = AVCaptureSession()
     captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
     let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
@@ -56,10 +61,33 @@ class CameraVC: UIViewController {
         previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
         previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
         cameraView.layer.addSublayer(previewLayer)
+        cameraView.addGestureRecognizer(tap)
         captureSession.startRunning()
       }
     } catch {
       debugPrint("could not setup camera :", error.localizedDescription)
+    }
+  }
+  
+  @objc func didTapCameraView() {
+    let settings = AVCapturePhotoSettings()
+    let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first
+    //thumbnail size
+    let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType, kCVPixelBufferWidthKey as String: 160, kCVPixelBufferHeightKey as String: 160]
+    settings.previewPhotoFormat = previewFormat
+    
+    cameraOutput.capturePhoto(with: settings, delegate: self)
+  }
+}
+
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+  func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+    if let error = error {
+      debugPrint(error)
+    } else {
+      photoData = photo.fileDataRepresentation()
+      let image = UIImage(data: photoData!)
+      self.capturedImgView.image = image
     }
   }
 }
