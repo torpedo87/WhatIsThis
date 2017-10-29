4e6649ead9359599eaf7e70e89768f8444dedc53
torpedo87 <bestofhand@naver.com>
2017-10-29 10:37:14 +0900

    init

---
+++ WhatIsThis/Controller/CameraVC.swift
@@ -0,0 +1,18 @@
+//
+//  CameraVC.swift
+//  WhatIsThis
+//
+//  Created by junwoo on 2017. 10. 29..
+//  Copyright © 2017년 samchon. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
